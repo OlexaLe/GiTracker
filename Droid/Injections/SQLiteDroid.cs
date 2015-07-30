45b3e313046bd29917134dbd49def860140a2512
Olexandr Leuschenko <olexa.le@gmail.com>
2015-07-30 11:14:02 +0300

    Android SQLite fixed

--- Droid/Injections/SQLiteDroid.cs
+++ Droid/Injections/SQLiteDroid.cs
@@ -10,11 +10,12 @@ namespace GiTracker.Droid.Injections
 {
     public class SQLiteDroid : ISQLite
     {
-        public SQLite.SQLiteConnection GetConnection () {
+        public SQLite.SQLiteConnection GetConnection ()
+        {
             var documentsPath = System.Environment.GetFolderPath (System.Environment.SpecialFolder.Personal);
-            var path = Path.Combine(documentsPath, Constants.DatabaseName);
-            return new SQLite.SQLiteConnection(path);
-        }}
+            var path = Path.Combine (documentsPath, Constants.DatabaseName);
+            return new SQLite.SQLiteConnection (path);
+        }
     }
 }
 

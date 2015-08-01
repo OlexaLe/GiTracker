d8c242207d66b1aa1247a27f1c7d11bca24ae353
foxanna <anna.domashych@gmail.com>
2015-08-01 23:54:53 +0200

    Introduce ApiService and model stubs

---
+++ GiTracker/Services/Api/IGitApiService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using GiTracker.Models;
+
+namespace GiTracker.Services.Api
+{
+    interface IGitApiService
+    {
+        Task<IEnumerable<GitHubIssue>> GetIssuesAsync(CancellationToken cancellationToken);
+    }
+}

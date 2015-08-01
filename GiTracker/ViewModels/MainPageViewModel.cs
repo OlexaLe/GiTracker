a99dba298e391422e1f0c9d49afc46fbddd7ddff
foxanna <anna.domashych@gmail.com>
2015-08-01 23:48:00 +0200

    Introduce BaseViewModel

--- GiTracker/ViewModels/MainPageViewModel.cs
+++ GiTracker/ViewModels/MainPageViewModel.cs
@@ -4,20 +4,15 @@ using GiTracker.Database;
 
 namespace GiTracker.ViewModels
 {
-    public class MainPageViewModel : BindableBase
+    public class MainPageViewModel : BaseViewModel
     {
         readonly IDatabaseService _databaseService;
 
         public MainPageViewModel (IDatabaseService databaseService)
         {
             _databaseService = databaseService; // JUST AN EXAMPLE!
-        }
 
-        string _title = "Main Page";
-
-        public string Title {
-            get { return _title; }
-            set { SetProperty (ref _title, value); }
+            Title = "Main Page";
         }
     }
 }

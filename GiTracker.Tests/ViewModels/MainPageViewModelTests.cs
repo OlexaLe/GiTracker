de28dd66c43ace0387e548d0e553b24d619d8ad8
Olexandr Leuschenko <ole@ciklum.com>
2015-08-27 10:27:00 +0300

    MainPage tests

---
+++ GiTracker.Tests/ViewModels/MainPageViewModelTests.cs
@@ -0,0 +1,54 @@
+﻿using GiTracker.Models;
+using GiTracker.ViewModels;
+using GiTracker.Views;
+using NUnit.Framework;
+
+namespace GiTracker.Tests.ViewModels
+{
+    [TestFixture]
+    public class MainPageViewModelTests
+    {
+        [Test]
+        public void DefaultPresentedViewModelTypeIsIssueList()
+        {
+            // Arrange
+            var vm = new MainPageViewModel(null);
+
+            // Act
+
+            // Assert
+            Assert.AreEqual(typeof (IssueList), vm.PresentedViewModelType);
+        }
+
+        [Test]
+        public async void PresentedViewModelTypeEventTriggeredOnRequest()
+        {
+            // Arrange
+            var vm = new MainPageViewModel(null);
+            var count = 0;
+            const int expectedCount = 1;
+            vm.PresentedViewModelTypeChanged += (sender, args) => count++;
+            var newPageType = typeof (IssueDetailsViewModel);
+
+            // Act
+            await vm.SlideMenuItemTapped.Execute(new SlideMenuItem {ScreenView = newPageType});
+
+            // Assert
+            Assert.AreEqual(expectedCount, count);
+        }
+
+        [Test]
+        public async void PresentedViewModelTypeShouldChange()
+        {
+            // Arrange
+            var vm = new MainPageViewModel(null);
+            var newPageType = typeof (IssueDetailsViewModel);
+
+            // Act
+            await vm.SlideMenuItemTapped.Execute(new SlideMenuItem {ScreenView = newPageType});
+
+            // Assert
+            Assert.AreEqual(newPageType, vm.PresentedViewModelType);
+        }
+    }
+}

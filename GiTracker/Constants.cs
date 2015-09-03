bc8743827f26bbaa5b200afd9c17c6c99c21cdd8
foxanna <anna.domashych@gmail.com>
2015-09-03 18:08:51 +0300

    Move company name from resources to constants

--- GiTracker/Constants.cs
+++ GiTracker/Constants.cs
@@ -4,6 +4,6 @@
     {
         public const string DatabaseName = "gitracker.dat";
         public const string XamarinGarageEmail = "xamarin.garage@gmail.com";
+        public const string XamarinGarage = "Xamarin Garage";
     }
 }
-

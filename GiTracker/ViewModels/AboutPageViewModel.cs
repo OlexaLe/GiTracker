a36c1710a33e355347994a5660584e7e61534a13
Olexandr Leuschenko <ole@ciklum.com>
2015-09-03 09:38:13 +0300

    Basic AboutPage created

---
+++ GiTracker/ViewModels/AboutPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using GiTracker.Helpers;
+using Prism.Navigation;
+
+namespace GiTracker.ViewModels
+{
+    public class AboutPageViewModel : BaseViewModel
+    {
+        private string _version;
+
+        public AboutPageViewModel(Loader loader, INavigationService navigationService) : base(loader, navigationService)
+        {
+            Version = "1.0";
+        }
+
+        public string Version
+        {
+            get { return _version; }
+            set { SetProperty(ref _version, value); }
+        }
+    }
+}

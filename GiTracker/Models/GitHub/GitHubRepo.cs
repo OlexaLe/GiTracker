c47d54b67c75a1e7d6b4bbdf90cb9e212946c58f
foxanna <anna.domashych@gmail.com>
2015-09-03 10:12:19 +0300

    Fill GitHubRepo model

--- GiTracker/Models/GitHub/GitHubRepo.cs
+++ GiTracker/Models/GitHub/GitHubRepo.cs
@@ -1,6 +1,13 @@
-﻿namespace GiTracker.Models.GitHub
+﻿using Newtonsoft.Json;
+
+namespace GiTracker.Models.GitHub
 {
     internal class GitHubRepo
     {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "full_name")]
+        public string Path { get; set; }
     }
 }
--- GiTracker/Models/IRepo.cs
+++ GiTracker/Models/IRepo.cs
@@ -2,5 +2,7 @@
 {
     public interface IRepo
     {
+        string Name { get; }
+        string Path { get; }
     }
 }

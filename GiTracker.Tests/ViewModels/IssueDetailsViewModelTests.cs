04038898213a4e4bd56f4c6898d5bdb1c0580309
Olexandr Leuschenko <ole@ciklum.com>
2015-09-03 10:43:05 +0300

    IssueDetail VM initialization unit tests

--- GiTracker.Tests/ViewModels/IssueDetailsViewModelTests.cs
+++ GiTracker.Tests/ViewModels/IssueDetailsViewModelTests.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using GiTracker.Helpers;
 using GiTracker.Models;
+using GiTracker.Resources.Strings;
 using GiTracker.Services.Device;
 using GiTracker.ViewModels;
 using Moq;
 using NUnit.Framework;
+using Prism.Navigation;
 
 namespace GiTracker.Tests.ViewModels
 {
@@ -35,8 +37,18 @@ namespace GiTracker.Tests.ViewModels
         public void ViewModelInitializesCorrectly()
         {
             // Arrange
+            const int expectedIssueNumber = 42;
+            var expectedPageTitle = string.Format(IssueDetails.IssueNumber, expectedIssueNumber);
+            var vm = new IssueDetailsViewModel(null, new Loader(null), null, null);
+            var issue = new IssueViewModel(new GitHubIssue {Number = expectedIssueNumber});
+            var parameters = new NavigationParameters {{IssueDetailsViewModel.IssueParameterName, issue}};
+
             // Act
+            vm.OnNavigatedTo(parameters);
+
             // Assert
+            Assert.AreEqual(expectedIssueNumber, vm.Issue.Number);
+            Assert.AreEqual(expectedPageTitle, vm.Title);
         }
     }
 }

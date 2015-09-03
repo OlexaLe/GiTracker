7d1b5a7f5ab84e8af8cc8676168fb2f178e2d56c
Olexandr Leuschenko <ole@ciklum.com>
2015-09-03 10:37:25 +0300

    IssueDetails page tests

---
+++ GiTracker.Tests/ViewModels/IssueDetailsViewModelTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using GiTracker.Helpers;
+using GiTracker.Models;
+using GiTracker.Services.Device;
+using GiTracker.ViewModels;
+using Moq;
+using NUnit.Framework;
+
+namespace GiTracker.Tests.ViewModels
+{
+    [TestFixture]
+    public class IssueDetailsViewModelTests
+    {
+        [Test]
+        public void OpenInBrowserWork()
+        {
+            // Arrange
+            var expectedIssuePage = "http://www.apple.com";
+            Uri calledUri = null;
+            var deviceService = new Mock<IDeviceService>();
+            deviceService.Setup(d => d.OpenUri(It.IsAny<Uri>())).Callback<Uri>(uri => calledUri = uri);
+            var vm = new IssueDetailsViewModel(deviceService.Object, new Loader(null), null, null);
+
+            var issue = new IssueViewModel(new GitHubIssue {WebPage = expectedIssuePage});
+            vm.Issue = issue;
+
+            // Act
+            vm.OpenInBrowserCommand.Execute(null);
+
+            // Assert
+            Assert.AreEqual(expectedIssuePage, calledUri.OriginalString);
+        }
+
+        [Test]
+        public void ViewModelInitializesCorrectly()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+}
--- GiTracker/ViewModels/IssueDetailsViewModel.cs
+++ GiTracker/ViewModels/IssueDetailsViewModel.cs
@@ -2,23 +2,25 @@
 using System.Windows.Input;
 using GiTracker.Helpers;
 using GiTracker.Resources.Strings;
+using GiTracker.Services.Device;
 using GiTracker.Services.Progress;
 using Prism.Commands;
 using Prism.Navigation;
-using Xamarin.Forms;
 
 namespace GiTracker.ViewModels
 {
     public class IssueDetailsViewModel : BaseViewModel
     {
         public const string IssueParameterName = "IssueParameterName";
+        private readonly IDeviceService _deviceService;
         private IssueViewModel _issue;
         private ICommand _openInBrowserCommand;
 
-        public IssueDetailsViewModel(Loader loader, IProgressService progressService,
+        public IssueDetailsViewModel(IDeviceService deviceService, Loader loader, IProgressService progressService,
             INavigationService navigationService)
             : base(loader, progressService, navigationService)
         {
+            _deviceService = deviceService;
         }
 
         public IssueViewModel Issue
@@ -38,6 +40,6 @@ namespace GiTracker.ViewModels
             Title = string.Format(IssueDetails.IssueNumber, Issue?.Number);
         }
 
-        private void OpenInBrowser() => Device.OpenUri(new Uri(Issue?.WebPage));
+        private void OpenInBrowser() => _deviceService.OpenUri(new Uri(Issue?.WebPage));
     }
 }

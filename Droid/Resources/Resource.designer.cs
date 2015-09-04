9a23f41220da719cc4b13b35f49eca1c25e0c47d
foxanna <anna.domashych@gmail.com>
2015-09-04 11:16:36 +0300

    Refactor Loader tests

--- Droid/Resources/Resource.designer.cs
+++ Droid/Resources/Resource.designer.cs
@@ -132,40 +132,37 @@ namespace GiTracker.Droid
 			public const int icon = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int monoandroidsplash = 2130837507;
+			public const int Octicon_Issue_Closed_Colored = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int Octicon_Issue_Closed_Colored = 2130837508;
+			public const int Octicon_Issue_Closed_White = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int Octicon_Issue_Closed_White = 2130837509;
+			public const int Octicon_Issue_Open_Colored = 2130837509;
 			
 			// aapt resource value: 0x7f020006
-			public const int Octicon_Issue_Open_Colored = 2130837510;
+			public const int Octicon_Issue_Open_White = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int Octicon_Issue_Open_White = 2130837511;
+			public const int Octicon_Issue_Unknown_Colored = 2130837511;
 			
 			// aapt resource value: 0x7f020008
-			public const int Octicon_Issue_Unknown_Colored = 2130837512;
+			public const int Octicon_Issue_Unknown_White = 2130837512;
 			
 			// aapt resource value: 0x7f020009
-			public const int Octicon_Issue_Unknown_White = 2130837513;
+			public const int Octicon_PullRequest_Colored = 2130837513;
 			
 			// aapt resource value: 0x7f02000a
-			public const int Octicon_PullRequest_Colored = 2130837514;
+			public const int Octicon_PullRequest_White = 2130837514;
 			
 			// aapt resource value: 0x7f02000b
-			public const int Octicon_PullRequest_White = 2130837515;
+			public const int Octicon_Repo_Colored = 2130837515;
 			
 			// aapt resource value: 0x7f02000c
-			public const int Octicon_Repo_Colored = 2130837516;
+			public const int roundedbg = 2130837516;
 			
 			// aapt resource value: 0x7f02000d
-			public const int roundedbg = 2130837517;
-			
-			// aapt resource value: 0x7f02000e
-			public const int roundedbgdark = 2130837518;
+			public const int roundedbgdark = 2130837517;
 			
 			static Drawable()
 			{
@@ -251,9 +248,6 @@ namespace GiTracker.Droid
 			
 			// aapt resource value: 0x7f050000
 			public const int GiTrackerTheme = 2131034112;
-			
-			// aapt resource value: 0x7f050001
-			public const int Mono_Android_Theme_Splash = 2131034113;
 			
 			static Style()
 			{

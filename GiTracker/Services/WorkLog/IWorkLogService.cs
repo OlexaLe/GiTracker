7d115e6b7a771731688a7dae9ffd13ead3121887
Olexandr Leuschenko <ole@ciklum.com>
2015-09-07 11:57:54 +0300

    LogTime method implemented

--- GiTracker/Services/WorkLog/IWorkLogService.cs
+++ GiTracker/Services/WorkLog/IWorkLogService.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using GiTracker.Models;
 
 namespace GiTracker.Services.WorkLog
 {
     public interface IWorkLogService
     {
-        Task LogTimeAsync(string repo, int issueId, DateTime logDate, TimeSpan logTime,
+        Task<IComment> LogTimeAsync(string repo, int issueId, DateTime logDate, TimeSpan logTime,
             CancellationToken cancellationToken);
     }
 }

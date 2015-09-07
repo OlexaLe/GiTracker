f774fae84a3167031300b176ff7afc5084d6e2c2
Olexandr Leuschenko <ole@ciklum.com>
2015-09-07 09:34:49 +0300

    LogTime method stub

--- GiTracker/Services/WorkLog/IWorkLogService.cs
+++ GiTracker/Services/WorkLog/IWorkLogService.cs
@@ -1,7 +1,11 @@
-﻿namespace GiTracker.Services.WorkLog
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GiTracker.Services.WorkLog
 {
     public interface IWorkLogService
     {
-        
+        Task LogTimeAsync(int issueId, DateTime logDate, TimeSpan logTime, CancellationToken cancellationToken);
     }
 }
--- GiTracker/Services/WorkLog/WorkLogService.cs
+++ GiTracker/Services/WorkLog/WorkLogService.cs
@@ -1,6 +1,14 @@
-﻿namespace GiTracker.Services.WorkLog
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GiTracker.Services.WorkLog
 {
     internal class WorkLogService : IWorkLogService
     {
+        public Task LogTimeAsync(int issueId, DateTime logDate, TimeSpan logTime, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

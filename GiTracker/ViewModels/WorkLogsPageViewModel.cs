baa6271921e4985dbd6da6328be9cc3d9b54baf1
foxanna <anna.domashych@gmail.com>
2015-09-08 10:58:09 +0300

    #28 Create view and viewmodel stubs

---
+++ GiTracker/ViewModels/WorkLogsPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using GiTracker.Models;
+using GiTracker.Resources.Strings;
+using GiTracker.Services.DataLoader;
+using GiTracker.Services.Progress;
+using GiTracker.Services.WorkLog;
+using Prism.Navigation;
+
+namespace GiTracker.ViewModels
+{
+    public class WorkLogsPageViewModel : BaseViewModel
+    {
+        private readonly IWorkLogService _workLogService;
+        private IIssue _issue;
+        private IRepo _repo;
+
+        public WorkLogsPageViewModel(ILoader loader,
+            IProgressService progressService,
+            INavigationService navigationService,
+            IWorkLogService workLogService)
+            : base(loader, progressService, navigationService)
+        {
+            _workLogService = workLogService;
+
+            Title = WorkLogs.Title;
+        }
+    }
+}
---
+++ GiTracker/Views/WorkLogsPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace GiTracker.Views
+{
+    public partial class WorkLogsPage : ContentPage
+    {
+        public WorkLogsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

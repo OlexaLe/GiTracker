6a66568b8d76b88a13072769557c031498b11900
evbo <evbo@ciklum.com>
2015-09-09 09:41:27 +0300

    Implemented login service

--- GiTracker/Services/Login/LoginService.cs
+++ GiTracker/Services/Login/LoginService.cs
@@ -1,13 +1,30 @@
-﻿using System;
+﻿using System.Threading;
 using System.Threading.Tasks;
+using GiTracker.Models;
+using GiTracker.Services.Api;
+using GiTracker.Services.Rest;
 
 namespace GiTracker.Services.Login
 {
     internal class LoginService : ILoginService
     {
-        public Task LoginAsync(string username, string password)
+        private readonly IGitApiProvider _gitApiProvider;
+        private readonly IRestService _restService;
+
+        public LoginService(IRestService restService, IGitApiProvider gitApiProvider)
         {
-            throw new NotImplementedException();
+            _restService = restService;
+            _gitApiProvider = gitApiProvider;
+        }
+
+        public async Task<IUser> LoginAsync(string username, string password)
+        {
+            var user =
+                await
+                    _restService.GetAsync(_gitApiProvider.GetLoginRequest(username, password), CancellationToken.None)
+                        .ConfigureAwait(false);
+
+            return user as IUser;
         }
     }
 }

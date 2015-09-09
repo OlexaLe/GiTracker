4a33cd2dd829d84a7d6f5ad15ad8c38d68078d06
evbo <evbo@ciklum.com>
2015-09-09 15:59:34 +0300

    Added credential service for base authentication

---
+++ GiTracker/Services/Credential/CredentialService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GiTracker.Services.Credential
+{
+    internal class CredentialService : ICredentialService
+    {
+        private string _basicAuthentication;
+
+        public Dictionary<string, string> Credential()
+        {
+            return new Dictionary<string, string> {{"Authorization", $"Basic {_basicAuthentication}"}};
+        }
+
+        public void SetBasicCredential(string username, string password)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes($"{username}:{password}");
+            _basicAuthentication = Convert.ToBase64String(plainTextBytes);
+        }
+    }
+}
---
+++ GiTracker/Services/Credential/ICredentialService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace GiTracker.Services.Credential
+{
+    internal interface ICredentialService
+    {
+        void SetBasicCredential(string username, string password);
+        Dictionary<string, string> Credential();
+    }
+}

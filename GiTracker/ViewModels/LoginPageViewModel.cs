eeb14d45353c519da3ee0ab5789fc821fc747c08
evbo <evbo@ciklum.com>
2015-09-09 09:42:28 +0300

    Added login service to login page

--- GiTracker/ViewModels/LoginPageViewModel.cs
+++ GiTracker/ViewModels/LoginPageViewModel.cs
@@ -1,14 +1,44 @@
 ﻿using GiTracker.Services.DataLoader;
+using GiTracker.Services.Login;
 using GiTracker.Services.Progress;
 using Prism.Navigation;
+using Xamarin.Forms;
 
 namespace GiTracker.ViewModels
 {
     internal class LoginPageViewModel : BaseViewModel
     {
-        public LoginPageViewModel(ILoader loader, IProgressService progressService, INavigationService navigationService)
+        private readonly ILoginService _loginService;
+        private string _login;
+        private Command _loginCommand;
+        private string _password;
+
+        public LoginPageViewModel(ILoader loader,
+            IProgressService progressService,
+            INavigationService navigationService,
+            ILoginService loginService)
             : base(loader, progressService, navigationService)
         {
+            _loginService = loginService;
+        }
+
+        public string Login
+        {
+            get { return _login; }
+            set { SetProperty(ref _login, value); }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { SetProperty(ref _password, value); }
+        }
+
+        public Command LoginCommand => _loginCommand ?? (_loginCommand = new Command(DoLogin));
+
+        private async void DoLogin()
+        {
+            await Loader.LoadAsync(async cancellationToken => { await _loginService.LoginAsync(Login, Password); });
         }
     }
 }

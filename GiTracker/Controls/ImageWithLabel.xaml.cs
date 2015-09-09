1e2768f9b01759a17df23fc0222b557037bbaaee
foxanna <anna.domashych@gmail.com>
2015-09-09 11:39:27 +0300

    ImageWithLabel via xaml

---
+++ GiTracker/Controls/ImageWithLabel.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Xamarin.Forms;
+
+namespace GiTracker.Controls
+{
+    public partial class ImageWithLabel : ContentView
+    {
+        public static readonly BindableProperty ImageUrlProperty =
+            BindableProperty.Create<ImageWithLabel, string>(p => p.ImageUrl, null);
+
+        public static readonly BindableProperty TextProperty =
+            BindableProperty.Create<ImageWithLabel, string>(p => p.Text, null);
+
+        public static readonly BindableProperty SecondaryTextProperty =
+            BindableProperty.Create<ImageWithLabel, string>(p => p.SecondaryText, null);
+
+        public ImageWithLabel()
+        {
+            InitializeComponent();
+        }
+
+        public string ImageUrl
+        {
+            get { return (string) GetValue(ImageUrlProperty); }
+            set { SetValue(ImageUrlProperty, value); }
+        }
+
+        public string Text
+        {
+            get { return (string) GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public string SecondaryText
+        {
+            get { return (string) GetValue(SecondaryTextProperty); }
+            set { SetValue(SecondaryTextProperty, value); }
+        }
+    }
+}

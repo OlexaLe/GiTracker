72a977333430384e2743334b4ab5edc39f378c86
evbo <evbo@ciklum.com>
2015-09-11 09:08:04 +0300

    Test for Credential Service

---
+++ GiTracker.Tests/Services/CredentialServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GiTracker.Services.Credential;
+using NUnit.Framework;
+
+namespace GiTracker.Tests.Services
+{
+    [TestFixture]
+    public class CredentialServiceTest
+    {
+        private readonly string _testName = "testname";
+        private readonly string _testPassword = "testpassword";
+
+        [Test]
+        public void CheckingBase64Credential()
+        {
+            // Arrange
+            var credentialService = new CredentialService();
+            var plainTextBytes = Encoding.UTF8.GetBytes($"{_testName}:{_testPassword}");
+            var basicAuthentication = Convert.ToBase64String(plainTextBytes);
+            credentialService.SetBasicCredential(_testName, _testPassword);
+
+            // Act
+            var credential = credentialService.Credential();
+
+            // Assert
+            Assert.AreEqual(credential,
+                new Dictionary<string, string> {{"Authorization", $"Basic {basicAuthentication}"}});
+        }
+    }
+}

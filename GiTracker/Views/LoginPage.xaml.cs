a9b7e85f880a177abbe4f8b318112fa2ef8e7c83
foxanna <anna.domashych@gmail.com>
2015-09-27 13:05:43 +0300

    Move focus to next input field and press login button when user hits enter

--- GiTracker/Views/LoginPage.xaml.cs
+++ GiTracker/Views/LoginPage.xaml.cs
@@ -1,10 +1,22 @@
-﻿namespace GiTracker.Views
+﻿using System;
+
+namespace GiTracker.Views
 {
     public partial class LoginPage : BasePage
     {
         public LoginPage()
         {
             InitializeComponent();
+        }
+
+        private void LoginEntry_OnCompleted(object sender, EventArgs e)
+        {
+            PasswordEntry.Focus();
+        }
+
+        private void PasswordEntry_OnCompleted(object sender, EventArgs e)
+        {
+            LoginButton.Command.Execute(LoginButton.CommandParameter);
         }
     }
 }

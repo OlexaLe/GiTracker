8d7a0ce900f29c21b2caf557af532b1b8e3ca7e6
Anna Domashych <anna.domashych@gmail.com>
2015-10-01 18:17:23 +0300

    Fix LoginServiceTests

--- GiTracker.Tests/Services/LoginServiceTest.cs
+++ GiTracker.Tests/Services/LoginServiceTest.cs
@@ -42,7 +42,7 @@ namespace GiTracker.Tests.Services
             var loginService = new LoginService(restServiceMoq.Object, _gitApiProvider, credentialServiceMoq.Object);
 
             // Act
-            var userResponce = await loginService.LoginAsync(_testName, _testPassword);
+            var userResponce = await loginService.LoginAsync(_testName, _testPassword, CancellationToken.None);
 
             // Assert
             credentialServiceMoq.Verify(moq => moq.SetCredentials(_testName, _testPassword),
@@ -66,7 +66,7 @@ namespace GiTracker.Tests.Services
             var loginService = new LoginService(restServiceMoq.Object, _gitApiProvider, credentialServiceMoq.Object);
 
             // Act
-            var userResponce = await loginService.LoginAsync(_testName, _testPassword);
+            var userResponce = await loginService.LoginAsync(_testName, _testPassword, CancellationToken.None);
 
             // Assert
             restServiceMoq.Verify(moq => moq.GetAsync(_userRequest, It.IsAny<CancellationToken>()),

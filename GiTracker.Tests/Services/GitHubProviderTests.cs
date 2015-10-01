cfb0188995baba42b8c9a34e6cc815d4c3cf8963
Anna Domashych <anna.domashych@gmail.com>
2015-10-01 18:19:25 +0300

    Rename Test to Tests for consistency

---
+++ GiTracker.Tests/Services/GitHubProviderTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using GiTracker.Services.Api;
+using GiTracker.Services.Credential;
+using Moq;
+using NUnit.Framework;
+
+namespace GiTracker.Tests.Services
+{
+    [TestFixture]
+    public class GitHubProviderTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _credentialsServiceMoq = new Mock<ICredentialsService>();
+            _credentialsServiceMoq.Setup(moq => moq.BasicAuthenticationToken).Returns(_basicAuthenticationToken);
+        }
+
+        private readonly string _basicAuthenticationToken = "TestBasicAuthenticationToken";
+
+        private Mock<ICredentialsService> _credentialsServiceMoq;
+
+        [Test]
+        public void CreateRequestCallsICredentialsServiceBasicAuthenticationToken()
+        {
+            // Arrange
+            var apiProvider = new GitHubApiProvider(_credentialsServiceMoq.Object);
+
+            // Act
+            var request = apiProvider.GetUserRequest();
+            var headers = request.DefaultHeaders;
+
+            // Assert
+            _credentialsServiceMoq.Verify(moq => moq.BasicAuthenticationToken, Times.AtLeastOnce);
+        }
+
+        [Test]
+        public void CreateRequestIsContainCredential()
+        {
+            // Arrange
+            var apiProvider = new GitHubApiProvider(_credentialsServiceMoq.Object);
+
+            // Act
+            var request = apiProvider.GetUserRequest();
+
+            // Assert
+            Assert.IsTrue(request.DefaultHeaders.Values.Any(value => value.Contains(_basicAuthenticationToken)));
+        }
+    }
+}
---
+++ GiTracker.Tests/Services/LoginServiceTests.cs
@@ -0,0 +1,78 @@
+﻿using System.Threading;
+using GiTracker.Models;
+using GiTracker.Services.Api;
+using GiTracker.Services.Credential;
+using GiTracker.Services.Login;
+using GiTracker.Services.Rest;
+using Moq;
+using NUnit.Framework;
+
+namespace GiTracker.Tests.Services
+{
+    [TestFixture]
+    public class LoginServiceTests
+    {
+        [SetUp]
+        public void Init()
+        {
+            var apiProviderMoq = new Mock<IGitApiProvider>();
+            apiProviderMoq.Setup(moq => moq.GetUserRequest()).Returns(_userRequest);
+            _gitApiProvider = apiProviderMoq.Object;
+        }
+
+        private IGitApiProvider _gitApiProvider;
+        private readonly IRestRequest _userRequest = Mock.Of<IRestRequest>();
+
+        private readonly string _testName = "testname";
+        private readonly string _testPassword = "testpassword";
+
+
+        [Test]
+        public async void GetUserCallsICredentialService()
+        {
+            // Arrange
+            var user = new Mock<IUser>().Object;
+
+            var restServiceMoq = new Mock<IRestService>();
+            restServiceMoq.Setup(moq => moq.GetAsync(It.IsAny<IRestRequest>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(user);
+
+            var credentialServiceMoq = new Mock<ICredentialsService>();
+
+            var loginService = new LoginService(restServiceMoq.Object, _gitApiProvider, credentialServiceMoq.Object);
+
+            // Act
+            var userResponce = await loginService.LoginAsync(_testName, _testPassword, CancellationToken.None);
+
+            // Assert
+            credentialServiceMoq.Verify(moq => moq.SetCredentials(_testName, _testPassword),
+                Times.Once);
+
+            Assert.AreEqual(user, userResponce);
+        }
+
+        [Test]
+        public async void GetUserCallsIRestService()
+        {
+            // Arrange
+            var user = new Mock<IUser>().Object;
+
+            var restServiceMoq = new Mock<IRestService>();
+            restServiceMoq.Setup(moq => moq.GetAsync(It.IsAny<IRestRequest>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(user);
+
+            var credentialServiceMoq = new Mock<ICredentialsService>();
+
+            var loginService = new LoginService(restServiceMoq.Object, _gitApiProvider, credentialServiceMoq.Object);
+
+            // Act
+            var userResponce = await loginService.LoginAsync(_testName, _testPassword, CancellationToken.None);
+
+            // Assert
+            restServiceMoq.Verify(moq => moq.GetAsync(_userRequest, It.IsAny<CancellationToken>()),
+                Times.Once);
+
+            Assert.AreEqual(user, userResponce);
+        }
+    }
+}
